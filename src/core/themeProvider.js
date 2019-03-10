50a315231fb4af984032249bc53b7c165c0d7f4a
Stefan Wegener <stefan.wegener@uninow.de>
2019-03-10 13:49:15 +0100

    :sparkles: useEffect Hook and AsyncStorage

--- src/core/themeProvider.js
+++ src/core/themeProvider.js
@@ -1,14 +1,24 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import THEMES from './themes.json';
 
+const STORAGE_KEY = 'THEME_ID';
 const ThemeContext = React.createContext();
 
 export const ThemeContextProvider = ({ children }) => {
   const [themeID, setThemeID] = useState(THEMES[1].key);
 
+  useEffect(() => {
+    (async () => {
+      const storedThemeID = await AsyncStorage.getItem(STORAGE_KEY);
+      if (storedThemeID) setThemeID(storedThemeID);
+    })();
+  }, []);
+
   return (
     <ThemeContext.Provider value={{ themeID, setThemeID }}>
-      {children}
+      {!!themeID ? children : null}
     </ThemeContext.Provider>
   );
 };
@@ -18,7 +28,10 @@ export function withTheme(Component) {
     const { themeID, setThemeID } = useContext(ThemeContext);
 
     const getTheme = themeID => THEMES.find(theme => theme.key === themeID);
-    const setTheme = themeID => setThemeID(themeID);
+    const setTheme = themeID => {
+      AsyncStorage.setItem(STORAGE_KEY, themeID);
+      setThemeID(themeID);
+    };
 
     return (
       <Component
